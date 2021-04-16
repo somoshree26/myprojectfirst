8253e5dda674e8a9a825b92afd853157325ada5b
SOMOSHREE  NANDI <56406827+somoshree26@users.noreply.github.com>
2021-04-16 21:16:37 +0530

    Update users.js

--- src/model/users.js
+++ src/model/users.js
@@ -1,1 +1,37 @@
-
+export default Users = [
+    {
+        id: 1, 
+        email: 'user1@email.com',
+        username: 'user1', 
+        password: 'password', 
+        userToken: 'token123'
+    },
+    {
+        id: 2, 
+        email: 'user2@email.com',
+        username: 'user2', 
+        password: 'pass1234', 
+        userToken: 'token12345'
+    },
+    {
+        id: 3, 
+        email: 'testuser@email.com',
+        username: 'somoshree', 
+        password: 'kiit123', 
+        userToken: 'testtoken'
+    },
+    {
+        id: 4, 
+        email: 'nuska@email.com',
+        username: 'anuska', 
+        password: 'kiit1234', 
+        userToken: 'testtoken'
+    },
+    {
+        id: 5, 
+        email: 'onon@email.com',
+        username: 'ananya', 
+        password: 'kiit12345', 
+        userToken: 'testtoken'
+    },
+];
